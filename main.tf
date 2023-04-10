7be142f6af058a4bae9ef693d40f849416c7b3e1
Reis-Silva J.C <cesar.everlastingtechnologydeveloper@outlook.com>
2023-04-09 21:56:12 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,3 +10,26 @@ provider "github" {
   owner = "Reis-Silva"
   token = "TOKEN_GITHUB" // Or use ENV
 }
+
+resource "github_actions_organization_permissions" "test" {
+  allowed_actions = "selected"
+  enabled_repositories = "selected"
+  allowed_actions_config {
+    github_owned_allowed = true 
+    patterns_allowed     = ["actions/cache@*", "actions/checkout@*"]
+    verified_allowed     = true
+  }
+}
+
+resource "github_repository" "example" {
+  name        = "devops"
+  description = "devops infnet"
+
+  visibility = "public"
+
+  template {
+    owner                = "github"
+    repository           = "terraform-template-module"
+    include_all_branches = true
+  }
+}
