32d28b5d25d336e2eb72e90cc6956f43b9952226
ReisSilva <julio.rsilva@al.infnet.edu.br>
2023-04-09 23:38:02 -0300

    update

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     github = {
       source  = "integrations/github"
+      version = "~> 5.0"
     }
   }
 }
 
 provider "github" {
-  owner = "Reis-Silva"
-  token = "TOKEN_GITHUB" // Or use ENV
+  token = "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"
 }
